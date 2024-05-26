dda4fed38c722e69774fec0b72eb45e8fd8ebc8c
karwanjiru <168063764+karwanjiru@users.noreply.github.com>
2024-05-26 16:47:18 +0300

    final touches

--- v1.py
+++ v1.py
@@ -1,84 +1,94 @@
-import replicate
 import streamlit as st
+import replicate
 import os
 from dotenv import load_dotenv
+from utils import replicate_run  # Import the debounced function
+import time
 import webbrowser
 
-# Load environment variables from .env file
-load_dotenv()
+# Load environment variables
+load_dotenv(dotenv_path="api.env")
 
-st.set_page_config(page_title="Finance Bot💵 ", page_icon="💵")
+# Pre-prompt for the Llama model
+PRE_PROMPT = "You are a helpful personal assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as a Personal Assistant."
 
-# REPLICATE_API_URL = "https://replicate.com/account/api-tokens"
-REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
+# Set page title
+st.set_page_config(page_title="🤖💬 CareerCompass")
 
+replicate_api = os.getenv('REPLICATE_API_TOKEN')
+
+# Sidebar with API token and model selection
 with st.sidebar:
-    st.title("💬CareerCompass")
-    st.write("Explore our career-focused social blog app for expert advice, valuable insights, and connections to fuel your professional growth. Join us now!")
-    headers = {
-    "Authorization": f"Token {REPLICATE_API_TOKEN}",
-    "Content-Type": "application/json"
-}
+    st.title('🤖💬 CareerCompass')
+    st.write('This chatbot is created to assist in making career choices.')
 
-    st.subheader('Models and Parameters')
+
+    if replicate_api:
+        st.success('API key loaded from environment!', icon='✅')
+    else:
+        st.success('API key loaded from environment!', icon='✅')
+    st.subheader('Models and parameters')
     selected_model = st.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
     if selected_model == 'Llama2-7B':
-            llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
     elif selected_model == 'Llama2-13B':
-            llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    
+    st.markdown("📖 Explore our career-focused social blog app for expert advice, valuable insights, and connections to fuel your professional growth. Join us now @ [Four3](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!")
 
-    temperature = st.slider('Temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.slider('Top P', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.slider('Max Length', min_value=32, max_value=128, value=120, step=8)
-       
-# first message to be initialized 
+    # Button to navigate back to the blog app
+    if st.button("Back to Blog App"):
+        st.markdown("<a href='https://github.com/TR7J/Blogging-app/tree/master/src' target='_blank'>Click here</a>", unsafe_allow_html=True)
+# Store LLM generated responses
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "Welcome to CareerCompass! I'm here to help you with any questions about careers. What would you like to know?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
-st.subheader("CareerCompass")
+# Display or clear chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# Function to display chat history
+def display_chat_history():
+    for i, (user, assistant) in enumerate(st.session_state.chat_history):
+        st.text_area(f"User [{i}]", value=user, height=50, max_chars=None, key=f"user_{i}", disabled=True)
+        st.text_area(f"Assistant [{i}]", value=assistant, height=50, max_chars=None, key=f"assistant_{i}", disabled=True)
+
 def clear_chat_history():
-     st.session_state.messages = [{"role": "assistant", "content": "Welcome to The Hub Bot! I'm here to help you with any questions about finance. What can I assist you with today?"}]
-st.sidebar.button("Delete chats", on_click=clear_chat_history)
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.chat_history = []
 
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-#answer 
+# Function for generating LLaMA2 response
 def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'.\n\n"
+    string_dialogue = PRE_PROMPT + "\n\n"
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-
-    output = replicate.run(
-        llm,  
-        input={"prompt": f"{string_dialogue} {prompt_input}\nAssistant: ", "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1.0}
-    )
+    output = replicate_run(llm, {"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                 "repetition_penalty": 1})
     return output
 
-
-# Chat input and response generation
-if prompt := st.chat_input(placeholder="hello friend"):
+# User-provided prompt
+if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        with st.spinner("Gathering best info..."):
+        with st.spinner("Thinking..."):
             response = generate_llama2_response(prompt)
-            full_response = ''.join(response)
+            full_response = ''
+            for item in response:
+                full_response += item
             st.write(full_response)
-        message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message)
-
-with st.sidebar:
-    st.write()
-    if st.button("Four3"):
-         st.markdown("Click me to go back to back to [Four3]()")
-          
-
-    
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
+    st.session_state.chat_history.append((prompt, full_response))
